5f53435305fc1e4ac7a9964071abd4d11fd943c8
ryangray2 <ryangray@udel.edu>
2021-02-16 21:59:38 -0500

    Update trades.js

--- trades.js
+++ trades.js
@@ -1,8 +1,8 @@
 var indOffer1 = {
   team: ind,
-  receiveText: ["2021 2nd Round (#54)"],
+  receiveText: ["2021 2nd Round (#53)"],
   giveText: ["Sam Darnold", "2021 5th Round"],
-  receiveIndex: [[1, 21]],
+  receiveIndex: [[1, 20]],
   giveIndex: [[4, 1]],
   receivePlayer: [],
   givePlayer: [SamDarnold]
@@ -10,9 +10,9 @@ var indOffer1 = {
 
 var indOffer2 = {
   team: ind,
-  receiveText: ["2021 3rd Round (#84)", "2021 4th Round (#117)"],
+  receiveText: ["2021 3rd Round (#85)", "2021 4th Round (#127)"],
   giveText: ["Sam Darnold"],
-  receiveIndex: [[2, 19], [3, 21]],
+  receiveIndex: [[2, 19], [3, 19]],
   giveIndex: [],
   receivePlayer: [],
   givePlayer: [SamDarnold]
@@ -20,27 +20,27 @@ var indOffer2 = {
 
 var pitOffer1 = {
   team: pit,
-  receiveText: ["2021 Round 2 (#55)"],
+  receiveText: ["2021 Round 2 (#56)"],
   giveText: ["Sam Darnold", "2021 6th Round"],
-  receiveIndex: [[1, 22]],
-  giveIndex: [[5, 1]],
+  receiveIndex: [[1, 23]],
+  giveIndex: [[5, 14]],
   receivePlayer: [],
   givePlayer: [SamDarnold]
 }
 
 var pitOffer2 = {
   team: pit,
-  receiveText: ["2021 Round 3 (#87)", "2021 Round 4 (#118)", "2021 Round 6 (#191)"],
+  receiveText: ["2021 Round 3 (#88)", "2021 Round 4 (#130)", "2021 Round 6 (#211)"],
   giveText: ["Sam Darnold", "2021 6th Round"],
-  receiveIndex: [[2, 22], [3, 22], [5, 27]],
-  giveIndex: [[5, 1]],
+  receiveIndex: [[2, 22], [3, 22], [5, 22]],
+  giveIndex: [[5, 14]],
   receivePlayer: [],
   givePlayer: [SamDarnold]
 }
 
 var noOffer = {
   team: no,
-  receiveText: ["2021 Round 2 (#59)"],
+  receiveText: ["2021 Round 2 (#60)"],
   giveText: ["Sam Darnold"],
   receiveIndex: [[1, 27]],
   giveIndex: [],
@@ -50,9 +50,9 @@ var noOffer = {
 
 var sfOffer = {
   team: sf,
-  receiveText: ["2021 Round 2 (#43)"],
+  receiveText: ["2021 Round 2 (#44)"],
   giveText: ["Sam Darnold", "2021 Round 4"],
-  receiveIndex: [[1, 10]],
+  receiveIndex: [[1, 11]],
   giveIndex: [[3, 1]],
   receivePlayer: [],
   givePlayer: [SamDarnold]
@@ -62,9 +62,9 @@ darnoldTradeArr = [indOffer1, indOffer2, pitOffer1, pitOffer2, noOffer, sfOffer]
 
 var atlOffer = {
   team: atl,
-  receiveText: ["2021 Round 1 (#4)", "2021 Round 2 (#35)", "2021 Round 4 (#98)"],
+  receiveText: ["2021 Round 1 (#4)", "2021 Round 2 (#36)", "2021 Round 4 (#110)"],
   giveText: ["2021 Round 1 (#2)"],
-  receiveIndex: [[0, 3], [1, 2], [3, 2]],
+  receiveIndex: [[0, 3], [1, 3], [3, 3]],
   giveIndex: [[0, 1]],
   receivePlayer: [],
   givePlayer: []
@@ -72,9 +72,9 @@ var atlOffer = {
 
 var detOffer = {
   team: det,
-  receiveText: ["2021 Round 1 (#7)", "2021 Round 2 (#41)", "2021 Round 3 (#72)", "2021 Round 4 (#102)", "2022 Round 3"],
+  receiveText: ["2021 Round 1 (#7)", "2021 Round 2 (#39)", "2021 Round 3 (#71)", "2021 Round 4 (#113)", "2022 Round 3"],
   giveText: ["2021 Round 1 (#2)"],
-  receiveIndex: [[0, 6], [1, 8], [2, 7], [3, 6]],
+  receiveIndex: [[0, 7], [1, 6], [2, 6], [3, 5]],
   giveIndex: [[0, 1]],
   receivePlayer: [],
   givePlayer: []
@@ -82,9 +82,9 @@ var detOffer = {
 
 var carOffer = {
   team: car,
-  receiveText: ["2021 Round 1 (#8)", "2021 Round 2 (#39)", "2021 Round 3 (#73)", "2021 Round 4 (#103)", "2022 Round 2"],
+  receiveText: ["2021 Round 1 (#8)", "2021 Round 2 (#40)", "2021 Round 3 (#72)", "2021 Round 4 (#114)", "2022 Round 2"],
   giveText: ["2021 Round 1 (#2)"],
-  receiveIndex: [[0, 6], [1, 6], [2, 8], [3, 7]],
+  receiveIndex: [[0, 7], [1, 7], [2, 7], [3, 6]],
   giveIndex: [[0, 1]],
   receivePlayer: [],
   givePlayer: []
@@ -92,9 +92,9 @@ var carOffer = {
 
 var denOffer = {
   team: den,
-  receiveText: ["2021 Round 1 (#9)", "2021 Round 2 (#40)", "2021 Round 3 (#71)", "2021 Round 4 (#105)", "2022 Round 2", "2022 Round 6"],
+  receiveText: ["2021 Round 1 (#9)", "2021 Round 2 (#41)", "2021 Round 3 (#73)", "2021 Round 4 (#115)", "2022 Round 2", "2022 Round 6"],
   giveText: ["2021 Round 1 (#2)"],
-  receiveIndex: [[0, 8], [1, 7], [2, 6], [3, 8]],
+  receiveIndex: [[0, 8], [1, 8], [2, 8], [3, 7]],
   giveIndex: [[0, 1]],
   receivePlayer: [],
   givePlayer: []
@@ -102,9 +102,9 @@ var denOffer = {
 
 var wasOffer = {
   team: was,
-  receiveText: ["2021 Round 1 (#19)", "2021 Round 2 (#51)", "2021 Round 3 (#74)", "2021 Round 3 (#83)", "2021 Round 4 (#115)", "2022 Round 1"],
+  receiveText: ["2021 Round 1 (#19)", "2021 Round 2 (#51)", "2021 Round 3 (#76)", "2021 Round 3 (#83)", "2021 Round 4 (#125)", "2022 Round 1"],
   giveText: ["2021 Round 1 (#2)"],
-  receiveIndex: [[0, 18], [1, 18], [2, 9], [2, 17], [3, 18]],
+  receiveIndex: [[0, 18], [1, 18], [2, 17], [2, 11], [3, 17]],
   giveIndex: [[0, 1]],
   receivePlayer: [],
   givePlayer: []
